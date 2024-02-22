d5d4114cb8fd888142a5db5e80699109036c71be
Dat Tran <datttrian@gmail.com>
2024-02-22 18:48:34 +0000

    Feb 22, 2024, 6:48 PM

--- src/exercise3/BusinessMortgage.java
+++ src/exercise3/BusinessMortgage.java
@@ -5,9 +5,3 @@ class BusinessMortgage extends Mortgage {
         super(mortgageNumber, customerName, mortgageAmount, currentPrimeRate + 0.01, term);
     }
 }
-
-class PersonalMortgage extends Mortgage {
-    public PersonalMortgage(int mortgageNumber, String customerName, double mortgageAmount, int term, double currentPrimeRate) {
-        super(mortgageNumber, customerName, mortgageAmount, currentPrimeRate + 0.02, term);
-    }
-}
--- src/exercise3/PersonalMortgage.java
+++ src/exercise3/PersonalMortgage.java
@@ -1,14 +1,7 @@
 package exercise3;
 
 class PersonalMortgage extends Mortgage {
-    public PersonalMortgage(int mortgageNumber, String customerName, double mortgageAmount, int term) {
-        super(mortgageNumber, customerName, mortgageAmount, term);
-        setInterestRate(0.02); // 2% over the current prime rate
-    }
-
-    @Override
-    public void setInterestRate(double rate) {
-        super.interestRate = rate;
+    public PersonalMortgage(int mortgageNumber, String customerName, double mortgageAmount, int term, double currentPrimeRate) {
+        super(mortgageNumber, customerName, mortgageAmount, currentPrimeRate + 0.02, term);
     }
 }
-
